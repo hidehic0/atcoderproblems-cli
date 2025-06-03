2d7be8dfe99a3c3826ffe085a58c218aef16bb3a
hidehic0 <hidehico.ooba@gmail.com>
2025-06-03 20:38:46 +0900

    init

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+type AcCountData struct {
+	Count int `json:"count"`
+	Rank  int `json:"rank"`
+}
+
+func getAcCount(username string) AcCountData {
+	url := "https://kenkoooo.com/atcoder/atcoder-api/v3/user/ac_rank?user=" + username
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data AcCountData
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("User not found")
+	}
+
+	return data
+}
+
+var rootCmd = &cobra.Command{
+	Use:  "example",
+	Long: "A tool that uses the atcoder problems API to use the atcoder problems function in CLI",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Help()
+		}
+
+		return nil
+	},
+}
+
+var acCountCmd = &cobra.Command{
+	Use:   "count",
+	Short: "Get ac count",
+	Long:  "count <username>",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// TODO: アニメーションを追加する
+		username := args[0]
+
+		showrank, err := cmd.Flags().GetBool("showrank")
+		if err != nil {
+			return err
+		}
+
+		data := getAcCount(username)
+
+		fmt.Print("AC count:" + strconv.Itoa(data.Count))
+
+		if showrank {
+			fmt.Print(" Rank:" + strconv.Itoa(data.Rank))
+		}
+
+		fmt.Println()
+
+		return nil
+	},
+}
+
+func main() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(acCountCmd)
+
+	acCountCmd.Flags().BoolP("showrank", "r", false, "show ac rank")
+}
